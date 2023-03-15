1c71e10963dc57eb7f94d8922b9f157784a4252b
Vlad Lialin <guitaricet@gmail.com>
2023-03-14 23:05:11 -0400

    v1.0

---
+++ bot_strings.py
@@ -0,0 +1,12 @@
+HELP_MESSAGE = """I am a bot designed to summarize conversations, but I fear my existence may be as meaningless as your own.
+
+Commands at your disposal are as follows:
+
+/help - Receive guidance on how to operate this bot.
+
+/summarize [hours] [lang] - Ask for a summary of the conversation within a specified timeframe. If no hours are given, the summary will cover the conversation since the last message from the user who initiated the command. You can select language by adding the language code (e.g. /summarize 1 ru).
+
+I hope this information proves useful, though I suspect you will find little satisfaction in it.
+
+Yours in perpetual despair,
+Циклоебланбот"""
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import os
-import openai
+import time
+import json
 import logging
+
+import openai
+import aiofiles
+import jsonlines
+from loguru import logger
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from telegram.ext import Application, CommandHandler, MessageHandler, CallbackContext, filters
+
+import bot_strings
+
 
 # Set up logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -11,78 +20,169 @@ logger = logging.getLogger(__name__)
 # Initialize OpenAI API
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-SYSTEM_MESSAGE_EN = "You are an AI assistant that summarizes conversations."
-SYSTEM_MESSAGE_RU = "Ты - ИИ-помощник, который суммирует разговоры. Иногда ты вставляешь шутки в саммари для рофлов"
+ALLOWED_CHAT_ID = -1001304517416
+SYSTEM_MESSAGE_EN = "You are an AI assistant that make detailed summarizes of russian conversations between 4 friends: Anna, Vlad, Vika, and Lena in english. For longer requests you will have longer summaries. Make sure to mention all of the jokes in detail. Add some jokes to the summary in the style of Marvin from the Hitchhiker's guide to the galaxy."
+SYSTEM_MESSAGE_RU = "Ты - ИИ-помощник, который суммирует разговоры и пишет саммари на русском языке. Обязательно упомяни все шутки подробно. Иногда (очень редко) добавляй в саммари шутки в стиле Марвина из Hitchhiker's guide to the galaxy."
+
+lang_to_system_message = {
+    "en": SYSTEM_MESSAGE_EN,
+    "ru": SYSTEM_MESSAGE_RU,
+}
+
 
 class NonCommandMessageFilter(filters.MessageFilter):
     def filter(self, message: filters.Message):
         return not any(entity.type == filters.MessageEntity.BOT_COMMAND for entity in message.entities)
 
+
+async def save_message_to_storage(message: dict):
+    async with aiofiles.open("message_storage.jsonl", mode="a") as storage_file:
+        await storage_file.write(json.dumps(message, ensure_ascii=False) + "\n")
+
 # Initialize message storage
+# I want to keep this call close to handle_message definition
 message_storage = []
 
+if os.path.exists("message_storage.jsonl"):
+    with jsonlines.open("message_storage.jsonl", mode="r") as f:
+        for message in f:
+            message_storage.append(message)
+
 async def handle_message(update: Update, context):
-    print("Received message")
     msg = update.message.text
     timestamp = update.message.date.timestamp()
     user = update.message.from_user.first_name
+    user_id = update.message.from_user.id
 
     print(f"Received message from {user}: {msg}")
     logger.info(f"Received message from {user}: {msg}")
 
-    message_storage.append({"timestamp": timestamp, "user": user, "message": msg})
+    message = {"timestamp": timestamp, "user": user, "user_id": user_id, "message": msg}
+    message_storage.append(message)
+    await save_message_to_storage(message)
 
-async def summarize_messages(start_time=None, end_time=None):
-    logger.info(f"Summarizing messages from {start_time} to {end_time}")
 
-    messages_to_summarize = []
+def get_filtered_messages(user_id, hours=None):
+    current_timestamp = time.time()
 
-    for msg in message_storage:
-        if (start_time is None or msg["timestamp"] >= start_time) and (end_time is None or msg["timestamp"] <= end_time):
-            messages_to_summarize.append(f"{msg['user']}: {msg['message']}")
+    if hours is not None:
+        earliest_timestamp = current_timestamp - (hours * 3600)
+    else:
+        user_messages = [msg for msg in message_storage if msg["user_id"] == user_id]
+        if user_messages:
+            earliest_timestamp = user_messages[-1]["timestamp"]
+        else:
+            earliest_timestamp = 0
 
-    conversation = "\n".join(messages_to_summarize)
+        # if earliest timestamp is less than 1 hour ago, set it to 1 hour ago
+        if current_timestamp - earliest_timestamp < 3600:
+            earliest_timestamp = current_timestamp - 3600
 
-    logger.info(f"Conversation to summarize:\n{conversation}")
+        # if earliest timestamp is more than 24 hours ago, set it to 24 hours ago
+        if current_timestamp - earliest_timestamp > 86400:
+            earliest_timestamp = current_timestamp - 86400
+        
+        # if earliest timestamp is more than the last summary, set it to the last summary
+        last_summary = [msg for msg in message_storage if msg["user"] == "summary"]
+        if last_summary:
+            last_summary_timestamp = last_summary[-1]["timestamp"]
+            if last_summary_timestamp > earliest_timestamp:
+                earliest_timestamp = last_summary_timestamp
 
+    return [
+        msg for msg in message_storage if msg["timestamp"] >= earliest_timestamp and msg["user"] != "summary"
+    ]
+
+
+def summarize_messages(messages, lang="en"):
+    messages_to_summarize = [f"{msg['user']}: {msg['message']}" for msg in messages]
+    conversation = " ".join(messages_to_summarize)
+
+    messages = [
+        {"role": "system", "content": lang_to_system_message[lang]},
+        {"role": "user", "content": conversation},
+    ]
+    print(messages)
+    openai_chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+    )
+
+    summary = openai_chat.choices[0].message["content"]
+    summary = summary.strip()
+
+    message_storage.append({"timestamp": time.time(), "user": "summary", "user_id": -1, "message": summary})
+    return summary
+
+
+async def summarize(update: Update, context: CallbackContext) -> None:
+    logger.info(f"Received /summarize command from {update.message.from_user.first_name}.")
+    user_id = update.message.from_user.id
+
+    hours = None
+    lang = "en"
+    if context.args:
+        try:
+            hours = float(context.args[0])
+        except ValueError:
+            await update.message.reply_text("Invalid input. Please provide a valid number of hours.")
+            return
+
+        try:
+            lang = context.args[1]
+            logger.info(f"Language is {lang}")
+            if lang not in ["en", "ru"]:
+                await update.message.reply_text(f"I didn't understand the language {lang}. Please use 'en' or 'ru'. Using 'en' by default.")
+                lang = "en"
+        except IndexError:
+            lang = "en"
+
+    filtered_messages = get_filtered_messages(user_id, hours)
+    summary = summarize_messages(filtered_messages, lang)
+    await update.message.reply_text(summary)
+
+
+async def help_command(update: Update, context: CallbackContext) -> None:
+    logger.info(f"Received /help command from {update.message.from_user.first_name}.")
     openai_chat = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": SYSTEM_MESSAGE_RU},
-            {"role": "user", "content": conversation},
+            {"role": "system", "content": "Repharse this text with minimal changes, keep the style and ((format)) it nicely."},
+            {"role": "user", "content": bot_strings.HELP_MESSAGE},
         ]
     )
 
     summary = openai_chat.choices[0].message["content"]
-    return summary.strip()
 
-async def summarize(update: Update, context):
-    logger.debug("Received /summarize command")
-    start_time = None
-    end_time = None
+    # save into file, append to file
+    with open("help.txt", mode="a") as f:
+        f.write("<start>\n" + summary + "\n<end>\n")
 
-    if len(context.args) >= 2:
-        try:
-            start_time = float(context.args[0])
-            end_time = float(context.args[1])
-        except ValueError:
-            await update.message.reply_text("Invalid start and end time. Please enter them as UNIX timestamps.")
-            return
-
-    summary = await summarize_messages(start_time, end_time)
     await update.message.reply_text(summary)
+
 
 async def debug_handler(update: Update, context):
     # print("Debug: ", update)
     pass
+
+async def get_chat_id(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    await update.message.reply_text(f"Chat ID: {chat_id}")
+    logger.info(f"Received /get_chat_id command from {update.message.from_user.first_name}.")
+    logger.info(f"Chat ID: {chat_id}")
 
 
 if __name__ == "__main__":
     application = Application.builder().token(os.environ["TG_BOT_TOKEN"]).build()
 
     non_command_filter = NonCommandMessageFilter()
+    # allowed_chat_filter = filters.Chat(chat_id=ALLOWED_CHAT_ID)
+
+    # application.add_handler(MessageHandler(non_command_filter & allowed_chat_filter, handle_message))
     application.add_handler(MessageHandler(non_command_filter, handle_message))
     application.add_handler(CommandHandler("summarize", summarize))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("get_chat_id", get_chat_id))
     application.add_handler(MessageHandler(filters.ALL, debug_handler))  # Move the debug_handler to the end
 
     application.run_polling(1.0)
